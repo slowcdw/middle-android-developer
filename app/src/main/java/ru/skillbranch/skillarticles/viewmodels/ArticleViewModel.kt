cc369464febc47f79e9afb9c7836e158545a695c
SlowCDW <1quick1@list.ru>
2020-08-01 00:21:15 +0300

    base done

---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -0,0 +1,171 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import ru.skillbranch.skillarticles.data.ArticleData
+import ru.skillbranch.skillarticles.data.ArticlePersonalInfo
+import ru.skillbranch.skillarticles.data.repositories.ArticleRepository
+import ru.skillbranch.skillarticles.extensions.data.toAppSettings
+import ru.skillbranch.skillarticles.extensions.data.toArticlePersonalInfo
+import ru.skillbranch.skillarticles.extensions.format
+
+class ArticleViewModel(val articleId: String): BaseViewModel<ArticleState>(ArticleState()) {
+    private val repository = ArticleRepository
+    private var menuIsShown:Boolean = false
+
+    init {
+        //subscriber on mutable data
+        subscribeOnDataSource(getArticleData()){ article, state ->
+            article ?: return@subscribeOnDataSource null
+            state.copy(
+                shareLink = article.shareLink,
+                title = article.title,
+                author = article.author,
+                category = article.category,
+                categoryIcon = article.categoryIcon,
+                date = article.date.format()
+            )
+        }
+
+        subscribeOnDataSource(getArticleContent()){ content, state ->
+            content ?: return@subscribeOnDataSource null
+            state.copy(
+                isLoadingContent = false,
+                content = content
+            )
+        }
+
+        subscribeOnDataSource(getArticlePersonalInfo()){ info, state ->
+            info ?: return@subscribeOnDataSource null
+            state.copy(
+                isBookmark = info.isBookmark,
+                isLike = info.isLike
+
+            )
+        }
+
+        //subscribe on setting
+       subscribeOnDataSource(repository.getAppSettings()){ setting, state ->
+           state.copy(
+               isDarkMode = setting.isDarkMode,
+               isBigText = setting.isBigText
+           )
+       }
+
+    }
+
+    //load text from network
+    private fun getArticleContent(): LiveData<List<Any>?>{
+        return repository.loadArticleContent(articleId)
+    }
+
+    //load data from mdb
+    private fun getArticleData(): LiveData<ArticleData?>{
+        return repository.getArticle(articleId)
+    }
+
+    //load data from db
+    private fun getArticlePersonalInfo(): LiveData<ArticlePersonalInfo?>{
+        return repository.loadArticlePersonalInfo(articleId)
+    }
+
+    //session state
+    fun handleToggleMenu(){
+        updateState {state ->
+            state.copy(isShowMenu = !state.isShowMenu).also { menuIsShown = !state.isShowMenu }
+        }
+    }
+
+    //app settings
+    fun handleNightMode(){
+        val settings = currentState.toAppSettings()
+        repository.updateSettings(settings.copy(isDarkMode = !settings.isDarkMode))
+    }
+
+    fun handleUpText(){
+        repository.updateSettings(currentState.toAppSettings().copy(isBigText = true))
+    }
+
+    fun handleDownText(){
+        repository.updateSettings(currentState.toAppSettings().copy(isBigText = false))
+    }
+
+    //personal article info
+    fun handleBookmark(){
+        val info = currentState.toArticlePersonalInfo()
+        repository.updateArticlePersonalInfo(info.copy(isBookmark = !info.isBookmark))
+
+        val msg = if (currentState.isBookmark) "Add to bookmark" else "Remove from bookmark"
+        notify(Notify.TextMessage(msg))
+    }
+
+
+
+
+    fun handleLike(){
+        Log.e("Article view model", "handle like: ");
+        val isLiked = currentState.isLike;
+        val toggleLike = {
+            val info = currentState.toArticlePersonalInfo()
+            repository.updateArticlePersonalInfo(info.copy(isLike = !info.isLike))
+        }
+        toggleLike()
+
+        val msg = if (!isLiked) Notify.TextMessage("Mark is liked")
+        else{
+            Notify.ActionMessage(
+                "Don`t like it anymore",
+                "No, still like it",
+                toggleLike
+            )
+        }
+        notify(msg)
+    }
+
+    //not implemented
+    fun handleShare(){
+        val msg = "Shared is not implemented"
+        notify(Notify.ErrorMessage(msg, "Ok", null))
+    }
+
+    fun hideMenu(){
+        updateState { it.copy(isShowMenu = false) }
+    }
+
+    fun showMenu(){
+        updateState { it.copy(isShowMenu = menuIsShown) }
+    }
+
+    fun handleSearchQuery(query: String?){
+        updateState { it.copy(searchQuery = query) }
+    }
+
+    fun handleIsSearch(isSearch: Boolean){
+        updateState { it.copy(isSearch = isSearch) }
+    }
+
+}
+
+data class ArticleState(
+    val isAuth: Boolean = false,
+    val isLoadingContent: Boolean = true,
+    val isLoadingReviews: Boolean = true,
+    val isLike: Boolean = false,
+    val isBookmark: Boolean = false,
+    val isShowMenu: Boolean = false,
+    val isBigText: Boolean = false,
+    val isDarkMode: Boolean = false,
+    val isSearch: Boolean = false,
+    val searchQuery: String? = null,
+    val searchResults: List<Pair<Int,Int>> = emptyList(),
+    val searchPosition: Int = 0,
+    val shareLink: String? = null,
+    val title: String? = null,
+    val category: String? = null,
+    val categoryIcon: Any? = null,
+    val date: String? = null,
+    val author: Any? = null,
+    val poster: String? = null,
+    val content: List<Any> = emptyList(),
+    val reviews: List<Any> = emptyList()
+)
