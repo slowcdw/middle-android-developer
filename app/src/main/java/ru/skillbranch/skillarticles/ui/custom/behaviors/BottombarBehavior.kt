956703e93a911b7e3f8ee2f7f3adce66b6b5184a
SlowCDW <1quick1@list.ru>
2020-08-08 19:41:55 +0300

    2 star_0

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -0,0 +1,36 @@
+package ru.skillbranch.skillarticles.ui.custom.behaviors
+
+import android.util.Log
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.math.MathUtils
+import androidx.core.view.ViewCompat
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
+
+class BottombarBehavior : CoordinatorLayout.Behavior<Bottombar>(){
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: Bottombar,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: Bottombar,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        child.translationY = MathUtils.clamp(child.translationY + dy, 0f, child.minHeight.toFloat())
+        Log.e("M_BottombarBehavior","dy : $dy translationY : ${child.translationY}")
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+    }
+
+}
